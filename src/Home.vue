4b2471804f93a91eea248014f38c2960e3e7d484
Pedro Oliveira <pedro.oliveira@wtvision.com>
2020-06-17 16:28:24 +0100

    added goToPortfolio; added goToHome;

--- src/Home.vue
+++ src/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="homeMainDiv">
+    <button @click="goToPortfolio">PORTFOLIO</button>
   </div>
 </template>
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  methods: {
+    goToPortfolio() {
+      this.$router.push({ name: "portfolio" });
+    }
+  }
 };
 </script>
 <style>
 body {
- background-image: url("./assets/back.png");
+  background-image: url("./assets/back.png");
 }
 </style>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="portfolioMainDiv">
     <h1>PORTFOLIO MAIN DIV</h1>
+    <button @click="goToHome">HOME</button>
   </div>
 </template>
 <script>
 export default {
-  name: "Portfolio"
+  name: "Portfolio",
+  methods: {
+    goToHome() {
+      this.$router.push({ name: "home" });
+    }
+  }
 };
 </script>
 <style>
